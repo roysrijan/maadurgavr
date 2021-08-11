840ddc1737c34efba76ae2fb03f9c2aec1f2a1a5
roysrijan <roy.srijan@outlook.com>
2021-08-11 15:35:55 +0530

    Backend Changes

--- amplify/backend/function/maadurgavr5b6c3980/src/index.js
+++ amplify/backend/function/maadurgavr5b6c3980/src/index.js
@@ -8,10 +8,10 @@ exports.handler = async (event) => {
     const response = {
         statusCode: 200,
     //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  }, 
+     headers: {
+         "Access-Control-Allow-Origin": "*",
+         "Access-Control-Allow-Headers": "*"
+     }, 
         body: JSON.stringify('Hello from Lambda!'),
     };
     return response;
