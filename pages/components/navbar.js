bb3537bdf9aa1ceb987e1c72ac793df03d034d9b
tanumayMatrix <tanumay@matrixnmedia.com>
2021-08-15 21:30:55 +0530

    some UI done

--- pages/components/navbar.js
+++ pages/components/navbar.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { Container, Row, Col, Navbar, Nav  } from "react-bootstrap";
+import { Container, Row, Col, Navbar, Nav, NavDropdown   } from "react-bootstrap";
 import stylesHome from "../../styles/Home.module.css";
 export default function TopNav() {
   return (
@@ -12,9 +12,17 @@ export default function TopNav() {
   <Navbar.Collapse id="responsive-navbar-nav" className={stylesHome.navWrap}>
     <Nav className="justify-content-center" >
       <Nav.Link href="/" className={stylesHome.navlist}>Home</Nav.Link>
-      <Nav.Link href="/about" className={stylesHome.navlist}>Tours</Nav.Link>
-      <Nav.Link href="/puja" className={stylesHome.navlist}>About</Nav.Link>
-      <Nav.Link href="/contact" className={stylesHome.navlist}>Blogs</Nav.Link>
+      {/* <Nav.Link href="/tours" className={stylesHome.navlist}>Tours</Nav.Link> */}
+      <NavDropdown title="Tours" id="basic-nav-dropdown" className={stylesHome.navlist}>
+          <NavDropdown.Item  href="/tours">Tours</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.2">2021</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3">2020</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3">2019</NavDropdown.Item>
+          
+        </NavDropdown>
+      <Nav.Link href="/about" className={stylesHome.navlist}>About</Nav.Link>
+      <Nav.Link href="/blog" className={stylesHome.navlist}>Blogs</Nav.Link>
+      <Nav.Link href="/contact" className={stylesHome.navlist}>Contacts</Nav.Link>
       <Nav.Link href="/share" className={stylesHome.navlistShare}>Share</Nav.Link>
       
     
