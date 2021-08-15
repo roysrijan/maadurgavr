371590c04a6b524232f0572c5fa2db19bc0b7bb1
roysrijan <roy.srijan@outlook.com>
2021-08-15 16:21:27 +0530

    Puja Count Down

--- pages/components/countdown.js
+++ pages/components/countdown.js
@@ -1,11 +1,12 @@
 import MyApp from "../_app";
 import Footer from "./footer";
 import TopNav from "./navbar";
+import Countdown from 'react-countdown'
 
 export default function CountDown() {
     return (
       <>
-        CountDown
+        <Countdown date={new Date('10/11/2021')} />
       </>
     )
 }
