1bbe0335df53ea578d32e9a83501928cc28006d4
roysrijan <roy.srijan@outlook.com>
2021-08-20 22:53:07 +0530

    UI style issue fix

--- pages/cms/dashboard/index.js
+++ pages/cms/dashboard/index.js
@@ -1,8 +1,12 @@
+import Head from "next/head";
 
 
 export default function dashboard() {
     return (
         <>
+            <Head>
+                <link href="../css/style.css" rel="stylesheet" />
+            </Head>
             <div class="container-scroller">
                 <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
                 <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
