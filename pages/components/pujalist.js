9c342dc5605a1b46b2b2a0b3676e4350c846650d
roysrijan <roy.srijan@outlook.com>
2021-10-13 19:03:54 +0530

    Thumbnail Share

--- pages/components/pujalist.js
+++ pages/components/pujalist.js
@@ -1,16 +1,23 @@
 import MyApp from "../_app";
 import Footer from "./footer";
 import TopNav from "./navbar";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, NavDropdown } from "react-bootstrap";
 import stylesHome from "../../styles/Home.module.css";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import Link from "next/link";
+import { FacebookShareButton, LinkedinShareButton, TwitterShareButton, WhatsappShareButton } from "react-share";
 
 export default function PujaList({pujalist, year, search}) {
   const [items, setItems] = useState(pujalist);
   let [count, setCount] = useState(0);
   const [currentYear, setCurrentYear] = useState(year);
+  const [shareUrl, setShareUrl] = useState();
+  const title = 'Durga Puja 2020'
+  useEffect(() => {
+    const host = window.location.href;
+    setShareUrl(`${host}`);
+  }, [shareUrl]);
   useEffect(async ()=>{
     if(currentYear!=year){
       let res = await fetch("https://lfhatz6o61.execute-api.ap-south-1.amazonaws.com/get-data?year="+year);
@@ -50,8 +57,41 @@ export default function PujaList({pujalist, year, search}) {
                 }
                 </div>
 
-                <a className={stylesHome.shareBt} href="">
-                  <img src="img/share-icon.png" alt="First slide" fluid />
+                <a className={stylesHome.shareBt}>
+                  <NavDropdown title={<img src="img/share-icon.png" alt="First slide" fluid />} id="basic-nav-dropdown" className={"shareBt"}>
+                  <LinkedinShareButton
+                            url={shareUrl+item.clubPageName}
+                            quote={title}
+                            className="Demo__some-network__share-button"
+                          >
+                  <NavDropdown.Item  href="#!"> <img src="img/001-linkedin.png" alt="First slide" title="Linkedin" fluid /></NavDropdown.Item>
+                  </LinkedinShareButton>
+                  <TwitterShareButton
+                            url={shareUrl+item.clubPageName}
+                            quote={title}
+                          >
+                  <NavDropdown.Item  href="#!"> <img src="img/002-twitter.png" alt="First slide" title="Twitter" fluid /></NavDropdown.Item>
+                  </TwitterShareButton>
+                  <FacebookShareButton
+                            url={shareUrl+item.clubPageName}
+                            quote={title}
+                            className="Demo__some-network__share-button"
+                          >
+                  <NavDropdown.Item  href="#!"> <img src="img/003-facebook.png" alt="First slide" title="Facebook" fluid /></NavDropdown.Item>
+                  </FacebookShareButton>
+                  <WhatsappShareButton
+                            url={shareUrl+item.clubPageName}
+                            quote={title}
+                            className="Demo__some-network__share-button"
+                          >
+                  <NavDropdown.Item  href="#!"> <img src="img/004-whatsapp.png" alt="First slide" title="Whatsapp" fluid /></NavDropdown.Item>
+                  </WhatsappShareButton>
+                  <NavDropdown.Item  href="#!"> <img src="img/link.png" alt="First slide" title="Copy Link" fluid /></NavDropdown.Item>
+                  
+                
+                  
+                </NavDropdown>
+
                 </a>
               </div>
             </Col>
